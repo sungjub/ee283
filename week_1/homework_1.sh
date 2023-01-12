85b46277adf83bc57a3e265672fcc9400001c4a3
sungjub <sungjub@uci.edu>
2023-01-12 13:44:58 -0800

    homework_1

---
+++ week_1/homework_1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --job-name=test    ## Name of the job.
+#SBATCH -A ecoevo283       ## account to charge
+#SBATCH -p standard        ## partition/queue name
+#SBATCH --cpus-per-task=1  ## number of cores the job needs
+
