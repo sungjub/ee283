328333231658997a91f12159db11fb78bc113110
sungjub <sungjub@uci.edu>
2023-01-17 23:08:53 -0800

    for the homework 1, week 1

--- week_1/homework_1.sh
+++ week_1/homework_1.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=test    ## Name of the job.
-#SBATCH -A ecoevo283       ## account to charge
-#SBATCH -p standard        ## partition/queue name
-#SBATCH --cpus-per-task=1  ## number of cores the job needs
 
+#SBATCH --job-name=homework_1 ## Name of the job 
+#SBATCH -A ecoevo283 ## account to charge 
+#SBATCH -p standard ## partition/queu names
+#SBATCH --nodes=1 ## (-N) number of nodes to use 
+#SBATCH --ntasks=1 ## (-n) number of tasks to lanuch 
+#SBATCH --cpus-per-task=1 ## number of cores the job needs
+
+# download the file
+wget https://wfitch.bio.uci.edu/~tdlong/problem1.tar.gz
+
+# uncompress the file
+tar -xvf problem1.tar.gz
+
+# show the 10th line of each file
+ head -10 problem1/p.txt | tail -1
+ head -10 problem1/f.txt | tail -1
